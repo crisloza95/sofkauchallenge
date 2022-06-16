24804c9b1ddfdfe3e443e5e5ca12ca91e69e2a38
crisloza95 <c.1406z@gmail.com>
2022-06-16 13:04:14 -0500

    terminado el back

--- src/main/java/com/sofkauchallenge/controllers/GameController.java
+++ src/main/java/com/sofkauchallenge/controllers/GameController.java
@@ -1,12 +1,10 @@
 package com.sofkauchallenge.controllers;
 
 import com.sofkauchallenge.Dtos.*;
+import com.sofkauchallenge.entities.Game;
 import com.sofkauchallenge.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -15,35 +13,39 @@ import java.util.List;
 public class GameController {
 
     @Autowired
-    GameService gameService;
+    private GameService gameService;
 
     @PostMapping("/newgame")
     public GameDto startGame(@RequestBody PlayerDto playerDto) {
         return gameService.startGame(playerDto);
     }
+//
+//    @PostMapping("/round")
+//    public RoundDto selectRound(@RequestBody GameDto gameDto) {
+//        return gameService.selectRound(gameDto);
+//    }
+//
+//    @PostMapping("/category")
+//    public CategoryDto selectCategory(@RequestBody RoundDto roundDto) {
+//        return gameService.selectCategory(roundDto);
+//    }
+//
+//    @PostMapping("/question")
+//    public QuestionDto randomQuestion(@RequestBody CategoryDto categoryDto) {
+//        return gameService.randomQuestion(categoryDto);
+//    }
+//
+//    @PostMapping("/answers")
+//    public List<AnswerDto> searchAnswers(@RequestBody QuestionDto questionDto) {
+//        return gameService.searchAnswers(questionDto);
+//    }
 
-    @PostMapping("/round")
-    public RoundDto selectRound(@RequestBody GameDto gameDto, CategoryDto categoryDto) {
-        int price = 12;
-        return gameService.selectRound(gameDto, categoryDto, price);
-    }
 
-    @PostMapping("/category")
-    public CategoryDto selectCategory(@RequestBody RoundDto roundDto) {
-        return gameService.selectCategory(roundDto);
-    }
-
-    @PostMapping("/question")
-    public QuestionDto randomQuestion(@RequestBody CategoryDto categoryDto) {
-        return gameService.randomQuestion(categoryDto);
-    }
-
-    @PostMapping("/answers")
-    public List<AnswerDto> searchAnswers(@RequestBody QuestionDto questionDto) {
+    @PostMapping("/game")
+    public List<AnswerDto> playGame(@RequestBody GameDto gameDto) {
+        var roundDto = gameService.selectRound(gameDto);
+        var categoryDto = gameService.selectCategory(roundDto);
+        var questionDto = gameService.randomQuestion(categoryDto);
         return gameService.searchAnswers(questionDto);
     }
-
-
-
-
 }
--- src/main/java/com/sofkauchallenge/services/GameService.java
+++ src/main/java/com/sofkauchallenge/services/GameService.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface GameService {
 
     public GameDto startGame(PlayerDto playerDto);
-    public RoundDto selectRound(GameDto gameDto, CategoryDto categoryDto, int price);
+    public RoundDto selectRound(GameDto gameDto);
     public CategoryDto selectCategory(RoundDto roundDto);
     public QuestionDto randomQuestion(CategoryDto categoryDto);
     public List<AnswerDto> searchAnswers(QuestionDto questionDto);
